347ef3c773f83c636dac4ae8eca96d6c39947002
neilime <escalle.emilien@gmail.com>
2016-11-05 21:20:27 +0100

    Fix travis config

--- build/publish_build.sh
+++ build/publish_build.sh
@@ -20,33 +20,37 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-echo -e "Publishing PHPDoc...\n"
+echo -e "1. Publishing PHPDoc\n"
 
 # Copy generated doc into $HOME
 cp -R build/phpdoc $HOME/phpdoc
 
 #  Retrieve branch gh-pages
+echo -e " * Retrieve branch gh-pages"
 cd $HOME
 git clone --quiet --branch=gh-pages $REPO gh-pages > /dev/null
-
 cd gh-pages
 
 # Remove old PHPDoc
 if [ -d ./phpdoc ]; then
+    echo -e " * Remove old PHPDoc"
     git rm -rf ./phpdoc
 fi
 
 ## Create new PHPDoc directory
+echo -e " * Create new PHPDoc directory"
 mkdir ./phpdoc
 cd ./phpdoc
 
-## Copy new PHPDoc version
+# Copy new PHPDoc version
+echo -e " * Copy new PHPDoc version"
 cp -Rf $HOME/phpdoc/* ./
 
 # Add, commit & push all files to git
+echo -e " * Add, commit & push all files to git"
 git add -f .
 git commit -m "PHPDocumentor (Publish Travis Build : $TRAVIS_BUILD_NUMBER)"
 git push -fq origin gh-pages > /dev/null
 
 # Done
-echo -e "Published PHPDoc to gh-pages.\n"
+echo -e " * Published PHPDoc to gh-pages.\n"
